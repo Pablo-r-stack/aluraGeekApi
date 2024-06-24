4b5b3c885cfb660369f63ddc8089eee087153137
Pablo <119269816+Pablo-r-stack@users.noreply.github.com>
2024-06-23 23:14:43 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,19 +1,31 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+// Create the tmp directory if it doesn't exist (ensures persistence within a single test run)
+const fs = require('fs');
+if (!fs.existsSync(`${process.cwd()}/tmp`)) {
+  fs.mkdirSync(`${process.cwd()}/tmp`);
+}
+
+// Use the FileSync adapter with the tmp directory path
+const adapter = new jsonServer.lowdb.adapters.FileSync(`${process.cwd()}/tmp/db.json`);
+const db = jsonServer.low(adapter);
+
+server.use(middlewares);
+
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/product/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
+  '/api/*': '/$1',
+  '/product/:resource/:id/show': '/:resource/:id'
+}));
+
+server.use(router);
+
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
